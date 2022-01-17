18967278d9b0f8797b920fc72e002f9ab6688fb0
waiyanlin81 <waiyanlin2991998@gmail.com>
2022-01-17 18:12:34 +0630

    add-table-and-view

--- database/migrations/2022_01_17_102804_create_employees_table.php
+++ database/migrations/2022_01_17_102804_create_employees_table.php
@@ -15,6 +15,18 @@ class CreateEmployeesTable extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('position');
+            $table->string('role');
+            $table->integer('age');
+            $table->string('email');
+            $table->string('password');
+            $table->string('image');
+            $table->string('phone');
+            $table->string('dob');
+            $table->string('address');
+            $table->bigInteger('department_id')->unsigned()->nullable();
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('set null');
             $table->timestamps();
         });
     }
---
+++ resources/views/common/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Employee Management System</title>
+  <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+  
+@section('name')
+    
+@endsection
+
+</body>
+</html>
