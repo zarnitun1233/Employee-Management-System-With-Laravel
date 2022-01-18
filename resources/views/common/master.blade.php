f6fbaf87d5640bdfe763c2e757cd29d81da4cc7d
Zarni Tun <zarnitun1233@gmail.com>
2022-01-18 16:18:30 +0630

    [development] conflict resolved

--- resources/views/common/master.blade.php
+++ resources/views/common/master.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,9 +9,11 @@
   <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
+
 <body>
-  
-@yield('content')
+
+  @yield('content')
 
 </body>
+
 </html>
