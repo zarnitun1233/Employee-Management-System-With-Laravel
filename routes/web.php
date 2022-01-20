795bf2ef8de17851181b59fc6e7af4974e273e70
Zarni Tun <zarnitun1233@gmail.com>
2022-01-20 16:33:20 +0630

    [devlopment] conflict solved

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\Leaves\LeavesController;
 use App\Models\Employee;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',function(){
-   return view('welcome');
+Route::get('/', function() {
+   return view('common.master');
 });
 
 
