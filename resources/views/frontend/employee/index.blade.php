1860c83525977cc242ac6ca7b988d5605036315d
Zarni Tun <zarnitun1233@gmail.com>
2022-01-26 15:58:38 +0630

    [development] finished design fix

--- resources/views/frontend/employee/index.blade.php
+++ resources/views/frontend/employee/index.blade.php
@@ -3,61 +3,60 @@
 @section('content')
 <div class="list-design">
   <div class="list-design-container">
-  <h1 class="list-title">Employee List</h1>
-  @if ($message = Session::get('success'))
-  <div>
-    <p class="show-alert">{{ $message }}</p>
+    <h1 class="list-title">Employee List</h1>
+    <div class="create-export">
+      <a href="{{ url('/employee/create') }}">Create Employee</a>
+      <a href="{{ url('/export') }}">Export</a>
+    </div>
+    @if ($message = Session::get('success'))
+    <div>
+      <p class="show-alert">{{ $message }}</p>
+    </div>
+    @endif
+    <table class="list-table">
+      <tr>
+        <th>No</th>
+        <th>Name</th>
+        <th>Position</th>
+        <th>Role</th>
+        <th>Age</th>
+        <th>Email</th>
+        <th>Image</th>
+        <th>Phone</th>
+        <th>DOB</th>
+        <th>Address</th>
+        <th>Department</th>
+        <th>Actions</th>
+      </tr>
+      @foreach($employees as $employee)
+      <tr>
+        <td>{{ $employee->id }}</td>
+        <td>{{ $employee->name }}</td>
+        <td>{{ $employee->position }}</td>
+        @if ($employee->role == 0)
+        <td>Employee</td>
+        @else
+        <td>Admin</td>
+        @endif
+        <td>{{ $employee->age }}</td>
+        <td>{{ $employee->email }}</td>
+        <td class="profile-home"><img src="{{ asset('images/' . $employee->image) }}" alt="Photo" width="70px" height="50px"></td>
+        <td>{{ $employee->phone }}</td>
+        <td>{{ $employee->dob }}</td>
+        <td>{{ $employee->address }}</td>
+        <td>{{ $employee->department->name }}</td>
+        <td>
+          <form action="{{ url('/employee/delete/'.$employee->id) }}" method="POST">
+            <a href="{{ url('/employee/edit/'.$employee->id) }}" class="list-edit">Edit</a>
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button class="list-delete">Delete</button>
+          </form>
+        </td>
+      </tr>
+      @endforeach
+    </table><br><br>
   </div>
-  @endif
-  <div class="create-export">
-    <a href="{{ url('/employee/create') }}">Create Employee</a>
-    <a href="{{ url('/export') }}">Export</a>
-  </div>
-  <br><br>
-  <table class="list-table">
-    <tr>
-      <th>No</th>
-      <th>Name</th>
-      <th>Position</th>
-      <th>Role</th>
-      <th>Age</th>
-      <th>Email</th>
-      <th>Image</th>
-      <th>Phone</th>
-      <th>DOB</th>
-      <th>Address</th>
-      <th>Department</th>
-      <th>Actions</th>
-    </tr>
-    @foreach($employees as $employee)
-    <tr>
-      <td>{{ $employee->id }}</td>
-      <td>{{ $employee->name }}</td>
-      <td>{{ $employee->position }}</td>
-      @if ($employee->role == 0)
-      <td>Employee</td>
-      @else
-      <td>Admin</td>
-      @endif
-      <td>{{ $employee->age }}</td>
-      <td>{{ $employee->email }}</td>
-      <td class="profile-home"><img src="{{ asset('images/' . $employee->image) }}" alt="Photo" width="70px" height="50px"></td>
-      <td>{{ $employee->phone }}</td>
-      <td>{{ $employee->dob }}</td>
-      <td>{{ $employee->address }}</td>
-      <td>{{ $employee->department->name }}</td>
-      <td>
-        <form action="{{ url('/employee/delete/'.$employee->id) }}" method="POST">
-          <a href="{{ url('/employee/edit/'.$employee->id) }}" class="list-edit">Edit</a>
-          {{ csrf_field() }}
-          {{ method_field('DELETE') }}
-          <button class="list-delete">Delete</button>
-        </form>
-      </td>
-    </tr>
-    @endforeach
-  </table><br><br>
   {{ $employees->links() }}
-  </div>
 </div>
 @endsection
--- resources/views/frontend/salary/index.blade.php
+++ resources/views/frontend/salary/index.blade.php
@@ -1,41 +1,45 @@
 @extends('common.master')
 
 @section('content')
-<h1 class="employee-list">Salary List</h1>
-@if ($message = Session::get('success'))
-<div>
-  <p class="employee-list-message">{{ $message }}</p>
-</div>
-@endif
-<div class="employee">
-  <a href="{{ url('/salary/create') }}">Create Salary</a><br><br>
-  <table class="employee-table">
-    <tr>
-      <th>No</th>
-      <th>Name</th>
-      <th>Position</th>
-      <th>Salary Amout</th>
-      <th>Date</th>
-      <th>Actions</th>
-    </tr>
-    @foreach($salaries as $salary)
-    <tr>
-      <td>{{ $salary->id }}</td>
-      <td>{{ $salary->employee->name }}</td>
-      <td>{{ $salary->employee->position }}</td>
-      <td>{{ $salary->amount }}</td>
-      <td>{{ $salary->date }}</td>
-      <td>
-        <form action="{{ url('/salary/delete/'.$salary->id) }}" method="POST">
-          <a href="{{ url('/salary/edit/'.$salary->id) }}" class="edit">Edit</a>
-          {{ csrf_field() }}
-          {{ method_field('DELETE') }}
-          <button class="delete">Delete</button>
-        </form>
-      </td>
-    </tr>
-    @endforeach
-  </table><br><br>
+<div class="list-design">
+  <div class="list-design-container">
+    <h1 class="list-title">Salary List</h1>
+    <div class="create-export">
+      <a href="{{ url('/employee/create') }}">Create Salary</a>
+    </div>
+    @if ($message = Session::get('success'))
+    <div>
+      <p class="show-alert">{{ $message }}</p>
+    </div>
+    @endif
+    <table class="list-table">
+      <tr>
+        <th>No</th>
+        <th>Name</th>
+        <th>Position</th>
+        <th>Salary Amout</th>
+        <th>Date</th>
+        <th>Actions</th>
+      </tr>
+      @foreach($salaries as $salary)
+      <tr>
+        <td>{{ $salary->id }}</td>
+        <td>{{ $salary->employee->name }}</td>
+        <td>{{ $salary->employee->position }}</td>
+        <td>{{ $salary->amount }}</td>
+        <td>{{ $salary->date }}</td>
+        <td>
+          <form action="{{ url('/salary/delete/'.$salary->id) }}" method="POST">
+            <a href="{{ url('/salary/edit/'.$salary->id) }}" class="list-edit">Edit</a>
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button class="list-delete">Delete</button>
+          </form>
+        </td>
+      </tr>
+      @endforeach
+    </table><br><br>
+  </div>
   {{ $salaries->links() }}
 </div>
 @endsection
