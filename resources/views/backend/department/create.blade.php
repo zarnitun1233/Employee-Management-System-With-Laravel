959f5d911b7d9369d15f3aeba62dbfa7c392fcec
mya-myintzu <myamyintzuthin12@gmail.com>
2022-01-29 11:44:07 +0630

    [development]design modification

--- resources/views/backend/department/create.blade.php
+++ resources/views/backend/department/create.blade.php
@@ -1,8 +1,10 @@
 @extends('common.master')
 
 @section('content')
-<h1 class="employee-create">Create Department</h1>
-<form action="" method="POST" enctype="multipart/form-data" class="employee-create-form">
+<h2 class="department-create-header">Create New Department</h2>
+<!--<form action="" method="POST" enctype="multipart/form-data" class="create-department-form">-->
+<div class="department-create-form">
+<table class="department-create-table">
   @csrf
   <label for="name">Department Name</label><br>
   <input type="text" id="name" name="name"><br>
@@ -14,7 +16,11 @@
   @error('position')
   <p class="validate-description-error">{{ $message }}</p>
   @enderror <br>
-  <input type="submit" name="submit" value="Create" class="create-department">
-  <a href="{{ url('/department/list') }}" class="back-create">Back</a>
+  <!--<input type="submit" name="submit" value="Create" class="create-department">
+  <a href="{{ url('/department/list') }}" class="back-create">Back</a>-->
+  <div class="btn">
+        <button type="submit"><a href="">Create</a></button>&nbsp;&nbsp;&nbsp;&nbsp;
+        <button type="cancel"><a href="{{ url('/department/list') }}">Back</a></button>
+        </div>
 </form>
 @endsection
