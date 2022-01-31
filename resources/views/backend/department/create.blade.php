0d693f554a43acb3d709a2206d0eb0dfd81c5b7d
Zarni Tun <zarnitun1233@gmail.com>
2022-01-31 09:42:43 +0630

    [development] department create fixed

--- resources/views/backend/department/create.blade.php
+++ resources/views/backend/department/create.blade.php
@@ -2,25 +2,30 @@
 
 @section('content')
 <h2 class="department-create-header">Create New Department</h2>
-<!--<form action="" method="POST" enctype="multipart/form-data" class="create-department-form">-->
-<div class="department-create-form">
-<table class="department-create-table">
-  @csrf
-  <label for="name">Department Name</label><br>
-  <input type="text" id="name" name="name"><br>
-  @error('name')
-  <p class="validate-department-error">{{ $message }}</p>
-  @enderror <br>
-  <label for="position">Description</label><br>
-  <input type="text" id="description" name="description"><br>
-  @error('position')
-  <p class="validate-description-error">{{ $message }}</p>
-  @enderror <br>
-  <!--<input type="submit" name="submit" value="Create" class="create-department">
-  <a href="{{ url('/department/list') }}" class="back-create">Back</a>-->
-  <div class="btn">
-        <button type="submit"><a href="">Create</a></button>&nbsp;&nbsp;&nbsp;&nbsp;
-        <button type="cancel"><a href="{{ url('/department/list') }}">Back</a></button>
-        </div>
+<form action="{{ url('/department/create') }}" method="POST" enctype="multipart/form-data" class="create-department-form">
+  <div class="department-create-form">
+    <table class="department-create-table">
+      @csrf
+      <label for="name">Department Name</label><br>
+      <input type="text" id="name" name="name"><br>
+      @error('name')
+      <p class="validate-department-error">{{ $message }}</p>
+      @enderror <br>
+      <label for="position">Description</label><br>
+      <input type="text" id="description" name="description"><br>
+      @error('description')
+      <p class="validate-description-error">{{ $message }}</p>
+      @enderror <br>
+      <div class="btn">
+        <button type="submit">Create</button>&nbsp;&nbsp;&nbsp;&nbsp;
+        <form action="{{ url('/department/list') }}" method="GET">
+          <button>Back</button>
+        </form>
+      </div>
+    </table>
+  </div>
 </form>
+<!--<div class="btn">
+<button onclick="history.back()">Back</button>
+</div>-->
 @endsection
