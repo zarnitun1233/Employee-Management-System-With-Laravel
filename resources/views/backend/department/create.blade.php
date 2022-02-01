9c06df1975627171b20f2d9ad46c70be5cd3da37
mya-myintzu <myamyintzuthin12@gmail.com>
2022-02-01 15:19:53 +0630

    [develpment] Conflit Modificaton

--- resources/views/backend/department/create.blade.php
+++ resources/views/backend/department/create.blade.php
@@ -1,28 +1,6 @@
 @extends('common.master')
 
 @section('content')
-<!--<h2 class="department-create-header">Create New Department</h2>
-<form action="{{ url('/department/create') }}" method="POST" enctype="multipart/form-data" class="create-department-form">
-  <div class="department-create-form">
-    <table class="department-create-table">
-      @csrf
-      <label for="name">Department Name</label><br>
-      <input type="text" id="name" name="name"><br>
-      @error('name')
-      <p class="validate-department-error">{{ $message }}</p>
-      @enderror <br>
-      <label for="position">Description</label><br>
-      <input type="text" id="description" name="description"><br>
-      @error('description')
-      <p class="validate-description-error">{{ $message }}</p>
-      @enderror <br>
-      <div class="btn">
-        <button type="submit">Create</button>&nbsp;&nbsp;&nbsp;&nbsp;
-        <a href="{{ url('/department/list') }}">Back</a>
-      </div>
-    </table>
-  </div>
-</form>-->
 <h2 class="employee-create-header">Create New Department</h2>
 <form action="" method="POST" enctype="multipart/form-data" class="employee-create-form">
   @csrf
