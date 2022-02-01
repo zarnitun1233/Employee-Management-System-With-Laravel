121cc9b5e9d3b773b452aea01673fbea1186418b
mya-myintzu <myamyintzuthin12@gmail.com>
2022-02-01 12:01:37 +0630

    [feature/development]Resetpassword Design

--- resources/views/frontend/Auth/reset-mail-send.blade.php
+++ resources/views/frontend/Auth/reset-mail-send.blade.php
@@ -5,12 +5,16 @@
 @if (session('message'))
     {{session('message')}}
 @endif
+<div class="reset-pswd">
+    <h2>Reset Password Form</h2>
     <form action="{{route('post.mail')}}" method="POST">
       @csrf
+      <label for="email">Email:</label>
       <input type="email" name="email"><br>
       @error('empId')
           <p>{{$message}}</p>
       @enderror
       <button type="submit">Send</button>
     </form>
+</div>
 @endsection
