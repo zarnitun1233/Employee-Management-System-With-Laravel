47125be95e03ffd2aa435945019f0f588575b1a1
Zarni Tun <zarnitun1233@gmail.com>
2022-02-02 22:10:18 +0630

    [development] change password design added

--- resources/views/frontend/Auth/change-password-form.blade.php
+++ resources/views/frontend/Auth/change-password-form.blade.php
@@ -1,22 +1,31 @@
 @extends('common.master')
 
-
 @section('content')
-<form action="{{ route('post.change.password') }}" method="POST">
+<h2 class="employee-create-header">Change New Password</h2>
+<form action="{{ route('post.change.password') }}" method="POST" enctype="multipart/form-data" class="employee-create-form">
   @csrf
-  <input type="hidden" name="email" value="{{ request()->email }}">
-  <input type="hidden" name="token" value="{{ request()->token ?? ' ' }}">
-  <label for="">Password</label>
-  <input type="password" name="password"><br>
-  @error('password')
-     <span>{{ $message }}</span>
-  @enderror
-  <label for="">Confirm Password</label>
-  <input type="password" name="password_confirmation"><br>
-  @error('password_confirmation')
-  <span>{{ $message }}</span>
-  @enderror
-  <button class="" type="submit">Update</button>
+  <table class="employee-table">
+    <input type="hidden" name="email" value="{{ request()->email }}">
+    <input type="hidden" name="token" value="{{ request()->token ?? ' ' }}">
+    <tr>
+      <td class="employee-label"><label for="">Password</label></td>
+      <td><input type="password" name="password"><br>
+        @error('password')
+        <span>{{ $message }}</span>
+        @enderror
+      </td>
+    </tr>
+    <tr>
+      <td class="employee-label"><label for="">Confirm Password</label></td>
+      <td><input type="password" name="password_confirmation"><br>
+        @error('password_confirmation')
+        <span>{{ $message }}</span>
+        @enderror
+      </td>
+    </tr>
+  </table>
+  <div class="btn">
+    <button type="submit">Update</button>&nbsp;&nbsp;&nbsp;&nbsp;
+  </div>
 </form>
-
 @endsection
